f861454249d1423357e7afe24be603862ba5e3f6
ademajd <drilonademaj@gmail.com>
2024-01-14 21:50:10 +0100

    added controlles

---
+++ alg-backend/controllers/showAllAccounts.js
@@ -0,0 +1,30 @@
+const userData = require("../models/users.json");
+const tweetData = require("../models/tweets.json");
+const moment = require("moment");
+
+const showAllAccounts = {
+  showAllAccounts: (req, res, next) => {
+    const sortedUsers = userData.sort(
+      (a, b) => new Date(a.created_at) - new Date(b.created_at)
+    );
+    const firstFiveAccounts = sortedUsers.map((user) => {
+      const userTweets = tweetData.filter((tweet) => tweet.user_id === user.id);
+      const numberOfTweets = userTweets.length;
+
+      const formattedDate = moment(user.created_at).format("D MMMM YYYY");
+
+      const fullName = `${user.first_name} ${user.last_name}`;
+
+      return {
+        date: formattedDate,
+        name: fullName,
+        id: user.id,
+        tweets: numberOfTweets,
+        status: user.isPremium ? "Premium" : "Normal",
+      };
+    });
+    res.json({ firstFiveAccounts });
+  },
+};
+
+module.exports = showAllAccounts;
--- alg-backend/routes/totalRoutes.js
+++ alg-backend/routes/totalRoutes.js
@@ -5,7 +5,14 @@ const TotalTweetsController = require("../controllers/totalTweets");
 const LastFiveAccountsController = require("../controllers/lastFiveAccounts");
 const FirstFiveAccountsController = require("../controllers/firstFiveAccounts");
 const LastFiveTweetsController = require("../controllers/lastFiveTweets");
-
+const FirstFiveId = require("../controllers/fistFiveId");
+const LastFiveId = require("../controllers/lastFiveId");
+const AllAccounts = require("../controllers/showAllAccounts");
+const AccountsByIdASC = require("../controllers/accountsByIdASC");
+const AccountsByIdDSC = require("../controllers/accountsByIdDSC");
+const AccountsByDateASC = require("../controllers/accountsByDateASC");
+const AccountsByDateDSC = require("../controllers/accountsByDateDSC");
+ 
 router.get("/total-accounts", TotalAccountsController.getTotalAccounts);
 router.get("/total-tweets", TotalTweetsController.getTotalTweets);
 router.get("/total-likes", TotalTweetsController.getTotalLikes);
@@ -18,8 +25,12 @@ router.get(
   FirstFiveAccountsController.getFirstFiveAccounts
 );
 router.get("/last-five-tweets", LastFiveTweetsController.getLastFiveTweets);
-=======
 router.get("/first-five-ids", FirstFiveId.getLastFiveAccounts);
 router.get("/last-five-ids", LastFiveId.getLastFiveAccounts);
+router.get("/show-all-accounts", AllAccounts.showAllAccounts);
+router.get("/accounts-id-asc", AccountsByIdASC.accountsByIdASC);
+router.get("/accounts-id-dsc", AccountsByIdDSC.accountsByIdDSC);
+router.get("/accounts-date-asc", AccountsByDateASC.accountsByDateASC);
+router.get("/accounts-date-dsc", AccountsByDateDSC.accountsByDateDSC);
 
 module.exports = router;
